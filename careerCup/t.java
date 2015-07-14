b0a4bfeeb84177450309be862de8978df333d104
mchundi <manjusha.chundi@gmail.com>
2015-07-14 16:01:46 +0530

    Update

---
+++ careerCup/t.java
@@ -0,0 +1,33 @@
+package careerCup;
+
+public class t {
+	public static void main(String[] args) {
+		System.out.println("23:59:59");
+		System.out.println("2:13:1");
+		System.out.println("2:3:1");
+		System.out.println("25:00:00");
+		System.out.println("");
+		System.out.println("23:60:00");
+		System.out.println("23:16:60");
+		System.out.println("23:00:");
+		System.out.println("23::16");
+		System.out.println("::");
+		System.out.println(" : : ");
+		System.out.println("0002:23:23");
+		System.out.println("23:23:2a");
+		System.out.println("-17:18:8");
+		System.out.println("23;23;59");
+		System.out.println("23;56:17");
+		System.out.println("2:1:23");
+		System.out.println("00:00:00");
+		System.out.println("(23:1:56)");
+		System.out.println("2-3-56");
+		System.out.println("12:-12:26");
+		System.out.println("23.6:6:56");
+		String str =" ";
+		System.out.println(str.contains(" "));
+		
+	}
+	
+	
+}
